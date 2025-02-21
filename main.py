22a9bcd376c68f078990f9f26a64c62d2dcbe62c
karabo <mawaiskara@gmail.com>
2025-02-21 09:40:46 +0200

    demo done

--- main.py
+++ main.py
@@ -3,44 +3,48 @@ import time
 
 moves = ["rock", "paper", "scissors", "lizard", "spock"]
 wins = {"rock":["scissors", "lizard"], "paper": ["rock","spock"], 
-        "scissor": ["paper", "lizard"], "lizard": ["spock", "paper"], 
+        "scissors": ["paper", "lizard"], "lizard": ["spock", "paper"], 
         "spock": ["rock", "scissors"]}
 
 cpu = random.choice(moves)
-player = input("rock paper scissors lizard spock\nchoose from the above\n>")
+while True:
+    player = input("rock paper scissors lizard spock\nchoose from the above\n>")
+    if player in moves:
+        break
+    else:
+        print("please choose from the available choices")
+        time.sleep(2)
 
 for x in wins:
     if cpu == x:
         if player in wins[x]:
             print("rock -- rock")
-            time.sleep(1)
+            time.sleep(0.5)
             print("paper -- paper")
-            time.sleep(1)
-            print("scissor -- scissor")
-            time.sleep(1)
-            print(f"cpu:{cpu}\n player:{player}")
-            time.sleep(1)
-            print(f"{cpu} beats {player}\n well done cpu!!")
+            time.sleep(0.5)
+            print(f"cpu:{cpu} -- player:{player}")
+            time.sleep(0.5)
+            print(f"{cpu} beats {player}\ncpu wins!!!")
             break
     elif player == x:
         if cpu in wins[x]:
             print("rock -- rock")
-            time.sleep(1)
+            time.sleep(0.5)
             print("paper -- paper")
-            time.sleep(1)
-            print("scissor -- scissor")
-            time.sleep(1)
-            print(f"cpu:{cpu}\n player:{player}")
-            time.sleep(1)
-            print(f"{player} beats {cpu}\n well done player!!")
+            time.sleep(0.5)
+            print(f"cpu:{cpu} -- player:{player}")
+            time.sleep(0.5)
+            print(f"{player} beats {cpu}\nplayer wins!!")
             break
-    else:
+    elif player == cpu:
         print("rock -- rock")
-        time.sleep(1)
+        time.sleep(0.5)
         print("paper -- paper")
-        time.sleep(1)
-        print("scissor -- scissor")
-        time.sleep(1)
-        print(f"cpu:{cpu}\n player:{player}")
-        time.sleep(1)
+        time.sleep(0.5)
+        print(f"cpu:{cpu} -- player:{player}")
+        time.sleep(0.5)
+        print("DRAW!")
+        break
+    else:
+        continue
 
